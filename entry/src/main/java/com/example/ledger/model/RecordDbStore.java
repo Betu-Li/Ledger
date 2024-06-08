a028e7393b3620dc29b5eb3ca86bb89080a0811f
Yuay <2831352144@qq.com>
2024-06-08 16:19:52 +0800

    项目创建和数据库配置

---
+++ entry/src/main/java/com/example/ledger/model/RecordDbStore.java
@@ -0,0 +1,9 @@
+package com.example.ledger.model;
+
+import ohos.data.orm.OrmDatabase;
+import ohos.data.orm.annotation.Database;
+
+@Database(entities = {RecordBean.class},version =1 )
+public abstract class RecordDbStore extends OrmDatabase {
+
+}
