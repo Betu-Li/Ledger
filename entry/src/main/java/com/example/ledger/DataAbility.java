cdc51a084832cb87a3da5cc01b7ba42619645763
Yuay <2831352144@qq.com>
2024-06-20 10:31:11 +0800

    数据库配置

--- entry/src/main/java/com/example/ledger/DataAbility.java
+++ entry/src/main/java/com/example/ledger/DataAbility.java
@@ -2,8 +2,10 @@ package com.example.ledger;
 
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.content.Intent;
+import ohos.data.DatabaseHelper;
+import ohos.data.dataability.DataAbilityUtils;
+import ohos.data.rdb.*;
 import ohos.data.resultset.ResultSet;
-import ohos.data.rdb.ValuesBucket;
 import ohos.data.dataability.DataAbilityPredicates;
 import ohos.hiviewdfx.HiLog;
 import ohos.hiviewdfx.HiLogLabel;
@@ -16,32 +18,107 @@ import java.io.*;
 public class DataAbility extends Ability {
     private static final HiLogLabel LABEL_LOG = new HiLogLabel(3, 0xD001100, "Demo");
 
+    private RdbStore rbdStore;
+    StoreConfig config = StoreConfig.newDefaultConfig("Bookkeeping.db");
+    RdbOpenCallback callback = new RdbOpenCallback() {
+        @Override
+        public void onCreate(RdbStore rbdStore) {
+            rbdStore.executeSql("CREATE TABLE IF NOT EXISTS records (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "year TEXT , month TEXT , day TEXT ," +
+                    "time TEXT ," +
+                    "amount REAL not null," +
+                    "type TEXT not null," +
+                    "category TEXT not null)");
+        }
+
+        @Override
+        public void onUpgrade(RdbStore rdbStore, int i, int i1) {
+
+        }
+    };
+
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         HiLog.info(LABEL_LOG, "DataAbility onStart");
+
+        // 初始化数据库连接
+        DatabaseHelper helper = new DatabaseHelper(this);
+        rbdStore = helper.getRdbStore(config, 1, callback);
     }
 
+    /**
+     * 重写查询方法
+     * @param uri 表示data ability的访问路径
+     * @param columns 传递查询的列名
+     * @param predicates 传递查询的条件
+     * @return 返回查询结果
+     */
     @Override
     public ResultSet query(Uri uri, String[] columns, DataAbilityPredicates predicates) {
-        return null;
+        RdbPredicates rdbPredicates = DataAbilityUtils.createRdbPredicates(predicates,"records");
+        return rbdStore.query(rdbPredicates, columns);
     }
 
+
+    /**
+     * 重写插入方法
+     * @param uri 表示data ability的访问路径
+     * @param value 传递操作数据的容器
+     * @return
+     * */
     @Override
     public int insert(Uri uri, ValuesBucket value) {
         HiLog.info(LABEL_LOG, "DataAbility insert");
-        return 999;
+        if (rbdStore == null) {
+            return 0;
+        }
+        ValuesBucket values = new ValuesBucket();
+        values.putString("year", value.getString("year"));
+        values.putString("month", value.getString("month"));
+        values.putString("day", value.getString("day"));
+        values.putString("time", value.getString("time"));
+        values.putDouble("amount", value.getDouble("amount"));
+        values.putString("type", value.getString("type"));
+        values.putString("category", value.getString("category"));
+        rbdStore.insert("records", values);
+        return 1;
     }
 
+    /**
+     * 重写删除方法
+     * @param uri 表示data ability的访问路径
+     * @param predicates 传递删除的条件
+     * @return
+     * */
     @Override
     public int delete(Uri uri, DataAbilityPredicates predicates) {
-        return 0;
+        RdbPredicates rdbPredicates = DataAbilityUtils.createRdbPredicates(predicates, "records");
+        return rbdStore.delete(rdbPredicates);
     }
 
+    /**
+     * 重写更新方法
+     * @param uri 表示data ability的访问路径
+     * @param value 传递操作数据的容器
+     * @param predicates 传递更新的条件
+     * @return
+     */
     @Override
     public int update(Uri uri, ValuesBucket value, DataAbilityPredicates predicates) {
-        return 0;
+        RdbPredicates rdbPredicates = DataAbilityUtils.createRdbPredicates(predicates, "records");
+        ValuesBucket values = new ValuesBucket();
+        values.putString("year", value.getString("year"));
+        values.putString("month", value.getString("month"));
+        values.putString("day", value.getString("day"));
+        values.putString("time", value.getString("time"));
+        values.putDouble("amount", value.getDouble("amount"));
+        values.putString("type", value.getString("type"));
+        values.putString("category", value.getString("category"));
+        return rbdStore.update(values, rdbPredicates);
     }
+
 
     @Override
     public FileDescriptor openFile(Uri uri, String mode) throws FileNotFoundException {
