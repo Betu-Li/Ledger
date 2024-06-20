269b8518fad6384922a98c7021731a4bb783afec
Yuay <2831352144@qq.com>
2024-06-20 17:59:38 +0800

    修改文件名

--- entry/src/main/java/com/example/ledger/MainAbility.java
+++ entry/src/main/java/com/example/ledger/MainAbility.java
@@ -1,6 +1,6 @@
 package com.example.ledger;
 
-import com.example.ledger.slice.AbilitySlice;
+import com.example.ledger.slice.BookSlice;
 
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.content.Intent;
@@ -11,8 +11,6 @@ public class MainAbility extends Ability {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
-//        super.setMainRoute(MainAbilitySlice.class.getName());
-//        super.setMainRoute(BookSlice.class.getName()); // 设置程序入口
-        super.setMainRoute(AbilitySlice.class.getName());
+        super.setMainRoute(BookSlice.class.getName());// 设置程序入口
     }
 }
